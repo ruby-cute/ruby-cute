6afdc76aaecce662dff80202bad9625ea1ca7dfd
Cristian Ruiz <cristian.ruiz@inria.fr>
2014-11-18 16:26:57 +0100

    Fixing timeout in wait_job method
    Change-Id: I09e2314e219b6f54b709f84e77928daa58bcdfff

---
+++ lib/cute/extensions.rb
@@ -0,0 +1,34 @@
+# Extends the class string for supporting timespan formats
+class String
+
+  def to_secs
+    #return self.to_f if s.is_a?(Numeric)
+    return Infinity if [ 'always', 'forever', 'infinitely' ].include?(self.to_s)
+    parts = self.to_s.split(':').map { |x| Integer(x) rescue nil }
+    if parts.all? && [ 2, 3 ].include?(parts.length)
+      secs = parts.zip([ 3600, 60, 1 ]).map { |x, y| x * y }.reduce(:+)
+      return secs
+    end
+    m = /^(\d+|\d+\.\d*)\s*(\w*)?$/.match(s)
+    num, unit = m.captures
+    mul = case unit
+          when '' then 1
+          when 's' then 1
+          when 'm' then 60
+          when 'h' then 60 * 60
+          when 'd' then 24 * 60 * 60
+          else nil
+          end
+    raise "Unknown timespan unit: '#{unit}' in #{self}" if mul.nil?
+    return num.to_f * mul
+  end
+
+  def to_time
+    secs = self.to_secs.to_i
+    minutes = secs / 60; secs %= 60
+    hours = minutes / 60; minutes %= 60
+    minutes += 1 if secs > 0
+    return '%.02d:%.02d' % [ hours, minutes ]
+  end
+
+end
--- lib/cute/g5k_api.rb
+++ lib/cute/g5k_api.rb
@@ -1,6 +1,7 @@
 require 'restclient'
 require 'yaml'
 require 'json'
+require 'pry'
 
 module Cute
 
@@ -28,9 +29,21 @@ module Cute
       return self['nodes']
     end
 
+    def rel(r)
+      return self['links'].detect { |x| x['rel'] == r }['href']
+    end
+
+    def rel_self
+      return rel('self')
+    end
+
     def __repr__
       return self['uid'] unless self['uid'].nil?
       return Hash[self.map { |k, v| [k, v.__repr__ ] }].to_s
+    end
+
+    def refresh(g5k)
+      return g5k.get_json(rel_self)
     end
 
     def self.parse(s)
@@ -44,7 +57,7 @@ module Cute
   class G5KRest
 
     attr_reader :user, :api
-
+    API_VERSION = "sid"
     # Initializes a REST connection
     # @param uri [String] resource identifier which normally is the url of the Rest API
     # @param user [String] user if authentication is needed
@@ -87,12 +100,22 @@ module Cute
       end
     end
 
+    # Creates a resource on the server
+    # @param path [String] this complements the URI to address to a specific resource
+    # @param json [Hash] contains the characteristics of the resources to be created.
+    def post_json(path, json)
+      r = resource(path).post(json.to_json,
+                              :content_type => "application/json",
+                              :accept => "application/json")
+      return G5KJson.parse(r)
+    end
+
     private
 
     # Test the connection and raises an error in case of a problem
     def test_connection
       begin
-        return get_json("")
+        return get_json("/#{API_VERSION}/")
         rescue RestClient::Unauthorized
           raise "Your Grid'5000 credentials are not recognized"
       end
@@ -104,6 +127,7 @@ module Cute
   # performs operations such as submitting jobs in the platform and deploying system images.
   class G5KUser
 
+    API_VERSION = "sid"
     # Initializes a REST connection for Grid'5000 API
     # @param uri [String] resource identifier which normally is the url of the Rest API
     # @param user [String] user if authentication is needed
@@ -125,7 +149,7 @@ module Cute
     # @return [Hash] all the status information of a given Grid'5000 site
     # @param site [String] a valid Grid'5000 site name
     def site_status(site)
-      @g5k_connection.get_json("sites/#{site}/status")
+      @g5k_connection.get_json(api_uri("sites/#{site}/status"))
     end
 
     # @return [Hash] the nodes state (e.g, free, busy, etc) that belong to a given Grid'5000 site
@@ -140,37 +164,37 @@ module Cute
       return nodes
     end
 
-    # @return [Hash] the description of all Grid'5000 sites
+    # @return [Array] the description of all Grid'5000 sites
     def sites
-      @g5k_connection.get_json("sites").items
+      @g5k_connection.get_json(api_uri("sites")).items
     end
 
-    # @return [Hash] the description of the clusters that belong to a given Grid'5000 site
+    # @return [Array] the description of the clusters that belong to a given Grid'5000 site
     # @param site [String] a valid Grid'5000 site name
     def clusters(site)
-      @g5k_connection.get_json("sites/#{site}/clusters").items
+      @g5k_connection.get_json(api_uri("sites/#{site}/clusters")).items
     end
 
-    # Returns all the jobs submitted in a given Grid'5000 site,
-    # if a uid is provided only the jobs owned by the user are shown.
+    # @returns [Hash] all the jobs submitted in a given Grid'5000 site,
+    #          if a uid is provided only the jobs owned by the user are shown.
     # @param site [String] a valid Grid'5000 site name
     # @param uid [String] user name in Grid'5000
     def get_jobs(site, uid = nil)
       filter = uid.nil? ? "" : "&user_uid=#{uid}"
-      @g5k_connection.get_json("sites/#{site}/jobs/?state=running#{filter}")
+      @g5k_connection.get_json(api_uri("/sites/#{site}/jobs/?state=running#{filter}"))
     end
 
     # @return [Hash] information concerning a given job submitted in a Grid'5000 site
     # @param site [String] a valid Grid'5000 site name
     # @param jid [Fixnum] a valid job identifier
     def get_job(site, jid)
-      @g5k_connection.get_json("sites/#{site}/jobs/#{jid}")
+      @g5k_connection.get_json(api_uri("/sites/#{site}/jobs/#{jid}"))
     end
 
     # @return [Hash] switches information available in a given Grid'5000 site.
     # @param site [String] a valid Grid'5000 site name
     def get_switches(site)
-      items = @g5k_connection.get_json("sites/#{site}/network_equipments").items
+      items = @g5k_connection.get_json("/sites/#{site}/network_equipments").items
       items = items.select { |x| x['kind'] == 'switch' }
       # extract nodes connected to those switches
       items.each { |switch|
@@ -192,6 +216,159 @@ module Cute
       s = get_switches(site).detect { |x| x.uid == name }
       raise "Unknown switch '#{name}'" if s.nil?
       return s
+    end
+
+    # @return a VLAN option codified to OAR.
+    # @param opts [Hash]
+    def handle_slash(opts)
+      slash = nil
+      predefined = { :slash_22 => 22, :slash_18 => 18 }
+      if opts[:slash]
+        bits = opts[:slash].to_i
+        slash = "slash_#{bits}=1"
+      else
+        slashes = predefined.select { |label, bits| opts.key?(label) }
+        unless slashes.empty?
+          label, bits = slashes.first
+          count = opts[label].to_i
+          slash = "slash_#{bits}=#{count}"
+        end
+      end
+      return slash
+    end
+
+    # helper for making the reservations the easy way
+    # @param opts [Hash] options compatible with OAR
+    def reserve_nodes(opts)
+
+      nodes = opts.fetch(:nodes, 1)
+      time = opts.fetch(:time, '01:00:00')
+      at = opts[:at]
+      slash = handle_slash(opts)
+      site = opts[:site]
+      type = opts.fetch(:type, :normal)
+      keep = opts[:keep]
+      name = opts.fetch(:name, 'rubyCute job')
+      command = opts[:cmd]
+      async = opts[:async]
+      ignore_dead = opts[:ignore_dead]
+      props = nil
+      vlan = opts[:vlan]
+      cluster = opts[:cluster]
+
+      raise 'At least nodes, time and site must be given'  if [nodes, time, site].any? { |x| x.nil? }
+
+      secs = time.to_secs
+      time = time.to_time
+
+      if nodes.is_a?(Array)
+        all_nodes = nodes
+        nodes = filter_dead_nodes(nodes) if ignore_dead
+        removed_nodes = all_nodes - nodes
+        info "Ignored nodes #{removed_nodes}." unless removed_nodes.empty?
+        hosts = nodes.map { |n| "'#{n}'" }.sort.join(',')
+        props = "host in (#{hosts})"
+        nodes = nodes.length
+      end
+
+      raise 'Nodes must be an integer.' unless nodes.is_a?(Integer)
+      # site = site.__repr__
+      raise 'Type must be either :deploy or :normal' unless (type.respond_to?(:to_sym) && [ :normal, :deploy ].include?(type.to_sym))
+      command = "sleep #{secs}" if command.nil?
+      type = type.to_sym
+
+      resources = "/nodes=#{nodes},walltime=#{time}"
+      resources = "{cluster='#{cluster}'}" + resources unless cluster.nil?
+      resources = "{type='kavlan'}/vlan=1+" + resources if vlan == true
+      resources = "#{slash}+" + resources unless slash.nil?
+
+      payload = {
+                 'resources' => resources,
+                 'name' => name,
+                 'command' => command
+                }
+
+      info "Reserving resources: #{resources} (type: #{type}) (in #{site})"
+
+
+      payload['properties'] = props unless props.nil?
+      if type == :deploy
+        payload['types'] = [ 'deploy' ]
+      else
+        payload['types'] = [ 'allow_classic_ssh' ]
+      end
+
+      unless at.nil?
+        dt = parse_time(at)
+        payload['reservation'] = dt
+        info "Starting this reservation at #{dt}"
+      end
+
+      begin
+        r = @g5k_connection.post_json(api_uri("sites/#{site}/jobs"),payload)  # This makes reference to the same class
+      rescue => e
+        info "Fail posting the json to the API"
+        raise
+      end
+
+      # it may be a different thread that releases reservations
+      # therefore we need to dereference proxy which
+      # in fact uses Thread.current and is local to the thread...
+
+      # I'm deactivating the code below because the engine method is
+      # defined in an Activity
+
+      # engine = proxy.engine
+
+      # engine.on_finish do
+      #   engine.verbose("Releasing job at #{r.rel_self}")
+      #   release(r)
+      # end if keep != true
+
+      job = @g5k_connection.get_json(r.rel_self)
+      job = wait_for_job(job) if async != true
+      return job
+
+    end
+
+    # wait for the job to be in a running state
+    # @param job [String] valid job identifier
+    # @param wait_time [Fixnum] wait time before raising an exception, default 10h
+    def wait_for_job(job,wait_time = 36000)
+
+      jid = job
+      info "Waiting for reservation #{jid}"
+      Timeout.timeout(wait_time) do
+        while true
+          job = job.refresh(@g5k_connection)
+          t = job['scheduled_at']
+          if !t.nil?
+            t = Time.at(t)
+            secs = [ t - Time.now, 0 ].max.to_i
+            info "Reservation #{jid} should be available at #{t} (#{secs} s)"
+          end
+          break if job['state'] == 'running'
+          raise "Job is finishing." if job['state'] == 'finishing'
+          Kernel.sleep(5)
+        end
+      end
+      info "Reservation #{jid} ready"
+      return job
+    end
+
+    private
+    # Handle the output of messages within the module
+    # @param msg [String] message to show
+    def info(msg)
+      if @logger.nil? then
+        t = Time.now
+        s = t.strftime('%Y-%m-%d %H:%M:%S.%L')
+        puts "#{s} => #{msg}"
+      end
+    end
+
+    def api_uri(path)
+      return "#{API_VERSION}/#{path}"
     end
 
   end
