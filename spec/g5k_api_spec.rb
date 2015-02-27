ba60a28251c13a9dcf827bfcfb2e3aa879eb5090
Cristian Ruiz <cristian.ruiz@inria.fr>
2015-02-27 11:01:58 +0100

    Rspec test rewritten using webmock
    Change-Id: I6406e943341d07bac9efa28f795198136e4e91dd

--- spec/g5k_api_spec.rb
+++ spec/g5k_api_spec.rb
@@ -1,112 +1,173 @@
 require 'spec_helper'
-
+# These tests can be executed either real or using mocking through WebMock
+# Real tests can be activated by setting the shell variable TEST_REAL
 describe Cute::G5K::API do
-  subject { Cute::G5K::API.new() }
+
+  subject { g5k = ENV['TEST_REAL'].nil?? Cute::G5K::API.new(:user => "test") : Cute::G5K::API.new() }
+
 
   let(:sites) { subject.site_uids}
-  let(:rand_site) { sites[rand(sites.length-1)]}
 
   before :each do
-    # @p = Cute::G5K::API.new()
-    # @sites = @p.site_uids
-    # #Choosing a random site
-    # @rand_site = @sites[rand(@sites.length-1)]
-    puts "Warning G5K_USER environment variable has to be defined for some tests" if ENV['G5K_USER'].nil?
+    # Choosing a random site based on the date
+    day = Time.now.day
+    index = ((day/31.to_f)*sites.length).to_i
+    index = 1 if index == 0
+    @rand_site = sites[index]
+    @env = subject.environment_uids(@rand_site).first
+    if ENV['TEST_REAL']
+      WebMock.disable!
+      puts "Testing in real Grid'5000 using site: #{@rand_site}"
+      puts "Warning G5K_USER environment variable has to be defined for some tests" if ENV['G5K_USER'].nil?
+    end
   end
 
-
-  it "should return an array with the site ids" do
-    expect(subject.site_uids.class).to eq(Array)
+  it "checks initialization of G5K::API" do
+    expect(subject.rest).to be_an_instance_of(Cute::G5K::G5KRest)
   end
 
-  it "It should return an array with the clusters ids" do
-    expect(subject.cluster_uids(rand_site).class).to eq(Array)
+  it "returns an array with the site ids" do
+    expect(sites.length).to be > 0
   end
 
-
-  it "consults all the sites for their clusters" do
-    clusters = subject.cluster_uids(rand_site)
+  it "returns an array with the clusters ids" do
+    clusters = subject.cluster_uids(@rand_site)
     expect(clusters.length).to be > 0
   end
 
-  it "should return a Json Hash with the status of a site" do
-    expect(subject.site_status(rand_site).class).to eq(Cute::G5K::G5KJSON)
+  it "returns a JSON Hash with the status of a site" do
+    expect(subject.site_status(@rand_site)).to be_an_instance_of(Cute::G5K::G5KJSON)
   end
 
-  it "should return a Hash with nodes status" do
-    expect(subject.nodes_status(rand_site).class).to eq(Hash)
-    expect(subject.nodes_status(rand_site).length).to be > 1
+  it "returns a Hash with nodes status" do
+    expect(subject.nodes_status(@rand_site)).to be_an_instance_of(Hash)
+    expect(subject.nodes_status(@rand_site).length).to be > 0
   end
 
-  it "should return an array with site status" do
-    expect(subject.sites.class).to eq(Cute::G5K::G5KArray)
+  it "returns an array with site status" do
+    expect(subject.sites).to be_an_instance_of(Cute::G5K::G5KArray)
   end
 
-  it "should not return any job" do
-    expect(subject.get_my_jobs(rand_site).empty?).to eq(true)
+  it "returns jobs in the site" do
+    expect(subject.get_jobs(@rand_site)).to be_an_instance_of(Array)
   end
 
-  it "should return the same information" do
-    #low level REST access
-    jobs_running = subject.rest.get_json("sid/sites/#{rand_site}/jobs/?state=running").items.length
-    expect(subject.get_jobs(rand_site,nil,"running").length).to eq(jobs_running)
+  it "return my_jobs in the site" do
+    expect(subject.get_my_jobs(@rand_site)).to be_an_instance_of(Array)
   end
 
-  it "should submit a job with subnet reservation" do
-    cluster = subject.cluster_uids(rand_site).first
-    job = subject.reserve(:site => rand_site, :nodes => 1, :walltime => '00:10:00',
-                          :subnets => [22,2], :cluster => cluster,:wait => false)
-    job = subject.wait_for_job(job, :wait_time => 600)
-    expect(job.class).to eq(Cute::G5K::G5KJSON)
-    sleep 1
-    # It verifies that the job has been submitted
-    expect(subject.get_my_jobs(rand_site).empty? && subject.get_my_jobs(rand_site,"waiting").empty?).to eq(false)
-    subnets = subject.get_subnets(job)
-    expect(subnets.first.class).to eq(IPAddress::IPv4)
-    expect(subnets.length).to eq(2)
+  it "returns all deployments" do
+    expect(subject.get_deployments(@rand_site)).to be_an_instance_of(Cute::G5K::G5KArray)
   end
 
-  it "should delete a job" do
-    # Deleting the job
-    subject.release_all(rand_site)
-    sleep 20
-    expect(subject.get_my_jobs(rand_site).empty? && subject.get_my_jobs(rand_site,"waiting").empty?).to eq(true)
+  it "raises a non found error" do
+    expect{subject.get_jobs("non-found")}.to raise_error(Cute::G5K::NotFound)
   end
 
-  it "should submit a job deploy" do
-    environment = subject.environment_uids(rand_site).first
-    job = subject.reserve(:site => rand_site,
-                     :nodes => 1, :walltime => '00:40:00',
-                     :env => environment)
+  it "raises a bad request error" do
+    expect{ subject.reserve(:site => @rand_site, :resources =>"/slash_22=1+{non_sense}")}.to raise_error(Cute::G5K::BadRequest)
+#    expect{ subject.reserve(:site => @rand_site, :resources =>"{ib30g='YES'}/nodes=2")}.to raise_error(Cute::G5K::BadRequest)
+  end
+
+  it "raises argument errors" do
+    job = Cute::G5K::G5KJSON.new
+    expect {subject.deploy(:env => "env")}.to raise_error(ArgumentError)
+    expect {subject.deploy(job)}.to raise_error(ArgumentError)
+    expect {subject.reserve(:non_existing => "site")}.to raise_error(ArgumentError)
+  end
+
+  it "raises invalid nodes format" do
+    job = Cute::G5K::G5KJSON.new
+    expect{subject.deploy(job,:env => "env")}.to raise_error(RuntimeError,"Unrecognized nodes format, use an Array")
+  end
+
+  it "raises error vlan" do
+    expect {subject.reserve(:site => @rand_site, :vlan => :nonsense)}.to raise_error(ArgumentError,'Option for vlan not recognized')
+  end
+
+
+  it "reserves and returns a valid job" do
+    job = subject.reserve(:site => @rand_site)
+    expect(job).to be_an_instance_of(Cute::G5K::G5KJSON)
+    subject.release(job)
+  end
+
+
+  it "reserves with vlan and get vlan hostnames" do
+    job = subject.reserve(:site => @rand_site, :nodes => 1, :type => :deploy, :vlan => :routed)
+    expect(subject.get_vlan_nodes(job)).to be_an_instance_of(Array)
+    subject.release(job)
+  end
+
+  it "gets subnets from job" do
+    job = subject.reserve(:site => @rand_site, :nodes => 1, :subnets => [22,2])
+    expect(subject.get_subnets(job).first).to be_an_instance_of(IPAddress::IPv4)
+    subject.release(job)
+  end
+
+
+  it "does not deploy immediately" do
+    job = subject.reserve(:site => @rand_site, :type => :deploy )
+    expect(job).to include("types" => ["deploy"])
+    expect(job).to_not have_key("deploy")
+  end
+
+  it "tests deploy with keys option" do
+    # Getting a deploy job
+    job = subject.get_my_jobs(@rand_site).select{ |j| j.has_value?(["deploy"])}.first
+    fake_key = Tempfile.new(["keys",".pub"])
+    fake_key_path = fake_key.path.split(".pub").first
+    expect(subject.deploy(job,:env => @env,:wait => true, :keys => fake_key_path)).to be_an_instance_of(Cute::G5K::G5KJSON)
+    fake_key.delete
+  end
+
+  it "waits for a deploy" do
+    job = subject.get_my_jobs(@rand_site).select{ |j| j.has_value?(["deploy"])}.first
     # It verifies that the job has been submitted with deploy
-    expect(subject.get_my_jobs(rand_site).empty? && subject.get_my_jobs(rand_site,"waiting").empty?).to eq(false)
-    expect(subject.get_my_jobs(rand_site).first["deploy"].empty?).to eq(false)
-    expect(subject.deploy_status(job).class).to eq(Array)
+    expect(subject.deploy_status(job)).to be_an_instance_of(Array)
     subject.wait_for_deploy(job)
-    expect(subject.deploy_status(job)).to eq(["terminated"])
+    expect(subject.deploy_status(job)).to be_an_instance_of(Array)
 
     #deploying again
-    subject.deploy(job, :env => subject.environment_uids(rand_site)[3])
-    subject.wait_for_deploy(job)
-    expect(subject.deploy_status(job)).to eq(["terminated","terminated"])
+    # subject.deploy(job, :env => @env)
+    # subject.wait_for_deploy(job)
+    # expect(subject.deploy_status(job).uniq).to eq(["terminated"])
   end
 
-  it "should delete a job" do
-    # Deleting the job
-    subject.release_all(rand_site)
-    sleep 20
-    expect(subject.get_my_jobs(rand_site).empty? && subject.get_my_jobs(rand_site,"waiting").empty?).to eq(true)
+
+  it "submits a job and then deploy" do
+    expect(subject.reserve(:site => @rand_site, :env => @env)).to have_key("deploy")
   end
+
+
+  it "returns the same information" do
+    #low level REST access
+    jobs_running = subject.rest.get_json("sid/sites/#{@rand_site}/jobs/?state=running").items.length
+    expect(subject.get_jobs(@rand_site,nil,"running").length).to eq(jobs_running)
+  end
+
+  it "submit and does not wait for the reservation" do
+    cluster = subject.cluster_uids(@rand_site).first
+    job = subject.reserve(:site => @rand_site, :wait => false)
+    job = subject.wait_for_job(job, :wait_time => 600)
+    expect(job).to include('state' => "running")
+  end
+
 
   it "should submit a job with OAR hierarchy" do
-   job1 = subject.reserve(:site => rand_site, :switches => 2, :nodes=>1, :cpus => 1, :cores => 1,
+
+   job1 = subject.reserve(:site => @rand_site, :switches => 2, :nodes=>1, :cpus => 1, :cores => 1,
                       :keys => "/home/#{ENV['G5K_USER']}/.ssh/id_rsa",:walltime => '00:10:00')
-   job2 = subject.reserve(:site => rand_site, :resources => "/switch=2/nodes=1/cpu=1/core=1",
+   job2 = subject.reserve(:site => @rand_site, :resources => "/switch=2/nodes=1/cpu=1/core=1",
                      :keys => "/home/#{ENV['G5K_USER']}/.ssh/id_rsa",:walltime => '00:10:00')
 
-   expect(subject.get_my_jobs(rand_site).length).to eq(2)
+   expect(job1).to be_an_instance_of(Cute::G5K::G5KJSON)
+   expect(job2).to be_an_instance_of(Cute::G5K::G5KJSON)
 
-   subject.release_all(rand_site)
+  end
+
+  it "releases all jobs in a site" do
+    expect(subject.release_all(@rand_site)).to be true
   end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,60 @@
+require 'simplecov'
+require 'webmock/rspec'
+
+SimpleCov.start
+# The SimpleCov.start must be issued before any of the application code is required!
 require 'cute'
+
+# Disabling all external requests
+WebMock.disable_net_connect!(allow_localhost: true)
+
+
+class FakeG5KResponse < Hash
+  MEDIA_TYPE = {'uid' => "1",
+                'id' => "1",
+                'user' => "test",
+                'items' => [{'uid' => "item1"},{'uid' => "item2"}],
+                'total' => [2],
+                'offset' => 2,
+                'links' => [{"rel" => "self","href" => "path", "type"=>"application/vnd.grid5000.collection+json"},
+                            {"rel" => "parent","href" => "path", "type"=>"application/vnd.grid5000.collection+json"}],
+                'state' => "running",
+                'started_at' => Time.now,
+                'created_at' => Time.now,
+                'status' => "terminated",
+                'types' => ["deploy"],
+                'assigned_nodes' => ["node1","node2"],
+                'resources_by_type' => {"res" => "val1","subnets" => ["10.140.0.0/22"], "vlans"=>["4"]},
+                'nodes' => {"node1" => {"hard"=> "alive", "soft"=>"busy"}}
+               }
+  def initialize(num_items = 2)
+    MEDIA_TYPE.each { |key,value| self[key] = value}
+    self['items'] = []
+    num_items.times.each{ self['items'].push(MEDIA_TYPE) }
+  end
+
+end
+
 
 RSpec.configure do |config|
   config.fail_fast = true
-  config.files_to_run = ["g5k_api_mock_spec.rb","g5k_api_spec.rb"]
-  config.mock_with :rspec do |c|
-    c.syntax = [:expect,:should]
+
+  g5k_media_type = FakeG5KResponse.new
+  # Example using addressable templates
+  # uri_sites = Addressable::Template.new "https://{user}:{password}@api.grid5000.fr/{version}/sites"
+  config.before(:each) do
+
+
+    stub_request(:any,/^https:\/\/.*\:.*@api.grid5000.fr\/.*/).
+      to_return(:status => 200, :body => g5k_media_type.to_json, :headers => {})
+
+    stub_request(:any,/^https:\/\/.*\:.*@api.grid5000.fr\/...\/sites\/non-found\/.*/).
+      to_return(:status => 404)
+    stub_request(:post, /^https:\/\/.*\:.*@api.grid5000.fr\/.*/).
+      with(:body => hash_including("resources" => "/slash_22=1+{non_sense},walltime=01:00")).
+      to_return(:status => 400)
+
+
   end
 
 end
