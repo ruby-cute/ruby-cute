0b78b29e197bd2480d4b7a6156bb592790978143
Cristian Ruiz <cristian.ruiz@inria.fr>
2015-03-02 14:27:31 +0100

    Updating execute class from Kadeploy
    Change-Id: Ia13263ee6e2b247466834d1ed9eebfe4a18a3d2b

--- lib/cute/execute.rb
+++ lib/cute/execute.rb
@@ -1,7 +1,8 @@
+# To be used as you're using Open3.popen3 in ruby 1.9.2
 class Execute
   require 'thread'
   require 'fcntl'
-  attr_reader :command, :exec_pid, :stdout, :stderr, :status
+  attr_reader :command, :exec_pid, :stdout, :stderr, :status,:emptypipes
   @@forkmutex = Mutex.new
 
   def initialize(*cmd)
@@ -12,216 +13,261 @@ class Execute
     @stdout = nil
     @stderr = nil
     @status = nil
+    @run_thread = nil
+    @killed = false
 
     @child_io = nil
     @parent_io = nil
+    @lock = Mutex.new
+    @emptypipes = false
   end
 
+  #Free the command, stdout stderr string.
   def free
     @command = nil
-    @exec_pid = nil
     @stdout = nil
     @stderr = nil
-    @status = nil
-    @child_io = nil
-    @parent_io = nil
   end
 
+  #Same as new function
   def self.[](*cmd)
     self.new(*cmd)
   end
 
+  #Initialize the pipes and return one array for parent and one array for child.
   def self.init_ios(opts={:stdin => false})
     if opts[:stdin]
       in_r, in_w = IO::pipe
       in_w.sync = true
-
-      out_r, out_w = IO::pipe
-      err_r, err_w = IO::pipe
-
-      [ [in_r,out_w,err_w], [in_w,out_r,err_r] ]
     else
-      out_r, out_w = IO::pipe
-      err_r, err_w = IO::pipe
-
-      [ [nil,out_w,err_w], [nil,out_r,err_r] ]
+      in_r, in_w = [nil,nil]
     end
+
+    out_r, out_w = opts[:stdout] == false ? [nil,nil] : IO::pipe
+    err_r, err_w = opts[:stderr] == false ? [nil,nil] : IO::pipe
+
+    [ [in_r,out_w,err_w], [in_w,out_r,err_r] ]
   end
 
+  # Launch the command provided by the constructor
+  #
+  # Arguments
+  #   opts: hash
+  #     :stdin, :stdout, :stderr  boolean to enable or disable pipe in respectively stdin,stdout and stderr.
+  #     :stdout_size, :stderr_size is number to limit the number of byte read by execute respectively on stdout or stderr.
   def run(opts={:stdin => false})
-    @@forkmutex.synchronize do
-      @child_io, @parent_io = Execute.init_ios(opts)
-      @parent_io.each { |io| io.fcntl(Fcntl::F_SETFD, Fcntl::FD_CLOEXEC) if io }
-      @exec_pid = fork {
-        @parent_io.each do |io|
-          begin
-            io.close if io and !io.closed?
-          rescue IOError
-          end
-        end
-
-        std = nil
-        if opts[:stdin]
-          std = [STDIN, STDOUT, STDERR]
-        else
-          std = [nil, STDOUT, STDERR]
-        end
-
-        std.each_index do |i|
-          next unless std[i]
-          std[i].reopen(@child_io[i])
-          begin
-            @child_io[i].close
-          rescue IOError
-          end
-        end
-        exec(*@command)
-      }
-
-      @child_io.each do |io|
+    @lock.synchronize do
+      if @run_thread
+        raise "Already launched"
+      else
         begin
-          io.close if io and !io.closed?
-        rescue IOError
+        ensure #We can't interrupt this process here before run was launched.
+          @child_io, @parent_io = Execute.init_ios(opts)
+          @@forkmutex.synchronize do
+            @exec_pid = fork do
+              run_fork()
+            end
+          end
+          @run_thread = Thread.new do
+            @child_io.each do |io|
+              io.close if io and !io.closed?
+            end
+            @child_io = nil
+            emptypipes = true
+
+            @stdout,emptypipes = read_parent_io(1,opts[:stdout_size],emptypipes)
+            @stderr,emptypipes = read_parent_io(2,opts[:stderr_size],emptypipes)
+
+            _, @status = Process.wait2(@exec_pid)
+            @exec_pid = nil
+
+            @parent_io.each do |io|
+              io.close if io and !io.closed?
+            end
+
+            @parent_io = nil
+            @emptypipes = emptypipes
+          end
         end
       end
     end
-    result = [@exec_pid, *@parent_io]
-    if block_given?
-      begin
-        ret = yield(*result)
-        wait(opts)
-        return ret
-      ensure
-        @parent_io.each do |io|
-          begin
-            io.close if io and !io.closed?
-          rescue IOError
-          end
-        end if @parent_io
-        @parent_io = nil
-        @child_io = nil
-      end
-    end
-    result
+    [@exec_pid, *@parent_io]
   end
 
-  def run!(opts={:stdin => false})
-    if block_given?
-      run(opts,Proc.new)
-    else
-      run(opts)
+  #Write to stdin
+  #Argument:
+  #  String passed to process stdin.
+  def write_stdin(str)
+    @lock.synchronize do
+      if @parent_io and @parent_io[0] and !@parent_io[0].closed?
+        @parent_io[0].write(str)
+      else
+        raise "Stdin is closed"
+      end
     end
+  end
+
+  # Close stdin of programme if it opened.
+  def close_stdin()
+    @lock.synchronize do
+      if @parent_io and @parent_io[0] and !@parent_io[0].closed?
+        @parent_io[0].close
+        @parent_io[0] = nil
+      end
+    end
+  end
+
+  # Run the command and return the Execute object.
+  # Arguments:
+  #   Opts see bellow
+  def run!(opts={:stdin => false})
+    run(opts)
     self
   end
 
-  # When :stdout_size or :stderr_size is given, if after have read the specified
-  # amount of data, the pipe is not empty, the 4th return value is set to false
+
+  # Wait the end of process
+  #
+  # Argument is hash
+  # :checkstatus : if it true at end of process it raises an exception if the result is not null.
+  #
+  # Output
+  # Array ( Process::Status, stdout String, stderr String, emptypipe).
   def wait(opts={:checkstatus => true})
-    unless @exec_pid.nil?
-      emptypipes = true
-      begin
-        begin
-          @parent_io[0].close if @parent_io[0] and !@parent_io[0].closed?
-        rescue IOError
-        end
-
-        if opts[:stdout_size] and opts[:stdout_size] > 0
-          @stdout = @parent_io[1].read(opts[:stdout_size]) unless @parent_io[1].closed?
-          emptypipes = false unless @parent_io[1].eof?
-          unless @parent_io[1].closed?
-            begin
-              @parent_io[1].readpartial(4096) while true
-            rescue EOFError
-            end
-          end
-        else
-          @stdout = @parent_io[1].read unless @parent_io[1].closed?
-        end
-
-        if opts[:stderr_size] and opts[:stderr_size] > 0
-          @stderr = @parent_io[2].read(opts[:stderr_size]) unless @parent_io[2].closed?
-          emptypipes = false unless @parent_io[2].eof?
-          unless @parent_io[2].closed?
-            begin
-              @parent_io[2].readpartial(4096) while true
-            rescue EOFError
-            end
-          end
-        else
-          @stderr = @parent_io[2].read unless @parent_io[2].closed?
-        end
-
-        _, @status = Process.wait2(@exec_pid)
-        @exec_pid = nil
-      rescue Errno::ECHILD
-        @status = nil
-      ensure
-        @parent_io.each do |io|
-          begin
-            io.close if io and !io.closed?
-          rescue IOError
-          end
-        end if @parent_io
-        @parent_io = nil
-        @child_io = nil
-      end
-
-      raise KadeployExecuteError.new(
-        "Command #{@command.inspect} exited with status #{@status.exitstatus}"
-      ) if opts[:checkstatus] and !@status.success?
-
-      [ @status, @stdout, @stderr, emptypipes ]
-    end
-  end
-
-  def self.kill_recursive(pid)
     begin
-      # SIGSTOPs the process to avoid it creating new children
+      wkilled=true
+      close_stdin()
+      @run_thread.join
+      wkilled=false
+    ensure
+      @lock.synchronize do
+        if wkilled && !@killed
+          kill!()
+        end
+      end
+      @run_thread.join
+      if !@killed
+        # raise SignalException if the process was terminated by a signal and the kill function was not called.
+        raise SignalException.new(@status.termsig) if @status and @status.signaled?
+        raise "Command #{@command.inspect} exited with status #{@status.exitstatus}" if opts[:checkstatus] and !@status.success?
+      end
+    end
+    [ @status, @stdout, @stderr, @emptypipes ]
+  end
+
+  EXECDEBUG = false
+  # kill a tree of processes. The killing is done in three steps:
+  # 1) STOP the target process
+  # 2) recursively kill all children
+  # 3) KILL the target process
+  def self.kill_recursive(pid)
+    puts "Killing PID #{pid} from PID #{$$}" if EXECDEBUG
+
+    # SIGSTOPs the process to avoid it creating new children
+    begin
       Process.kill('STOP',pid)
-      # Gather the list of children before killing the parent in order to
-      # be able to kill children that will be re-attached to init
-      children = `ps --ppid #{pid} -o pid=`.split("\n").collect!{|p| p.strip.to_i}
-      # Directly kill the process not to generate <defunct> children
+    rescue Errno::ESRCH # "no such process". The process was already killed, return.
+      puts "got ESRCH on STOP" if EXECDEBUG
+      return
+    end
+    # Gather the list of children before killing the parent in order to
+    # be able to kill children that will be re-attached to init
+    children = `ps --ppid #{pid} -o pid=`.split("\n").collect!{|p| p.strip.to_i}
+    children.compact!
+    puts "Children: #{children}" if EXECDEBUG
+    # Check that the process still exists
+    # Directly kill the process not to generate <defunct> children
+    children.each do |cpid|
+      kill_recursive(cpid)
+    end if children
+
+    begin
       Process.kill('KILL',pid)
-      children.each do |cpid|
-        kill_recursive(cpid)
-      end
-    rescue Errno::ESRCH
+    rescue Errno::ESRCH # "no such process". The process was already killed, return.
+      puts "got ESRCH on KILL" if EXECDEBUG
+      return
     end
   end
 
+  #Kill the launched process.
   def kill()
-    @parent_io.each do |io|
-      begin
-        io.close if io and !io.closed?
-      rescue IOError
-      end
-    end if @parent_io
-
-    @child_io.each do |io|
-      begin
-        io.close if io and !io.closed?
-      rescue IOError
-      end
-    end if @child_io
-
-    unless @exec_pid.nil?
-      begin
-        Execute.kill_recursive(@exec_pid)
-        Process.wait(@exec_pid)
-      rescue Errno::ESRCH
-      end
-      @exec_pid = nil
-    end
-    free()
+    @lock.synchronize{ kill! }
   end
 
-  def self.do(*cmd,&block)
-    exec = Execute[*cmd]
-    if block_given?
-      exec.run(nil,Proc.new)
-    else
-      exec.run()
+
+  private
+
+  #Launch kill_recurcive if in launched and it not already killed
+  #@killed becomes true.
+  def kill!()
+    if @exec_pid && !@killed
+      @killed = true
+      Execute.kill_recursive(@exec_pid)
+      # This function do not wait the PID since the thread that use wait() is supposed to be running and to do so
     end
+  end
+
+  #Read pipe and return out and boolean which indicate if pipe are empty.
+  #Arguments:
+  #  +num       : number of file descriptor
+  #  +size      : Maximum number of bytes must be read 0 is unlimited.
+  #  +emptypipes: Previous value of emptypipe the new value was obtained with logical and.
+  #Output:
+  #  Array (output: String, emptypipes: Boolean)
+  def read_parent_io(num,size,emptypipes)
+    out=''
+    if @parent_io and @parent_io[num]
+      if size and size > 0
+        out = @parent_io[num].read(size) unless @parent_io[num].closed?
+        emptypipes = false if !@parent_io[num].closed? and !@parent_io[num].eof?
+        unless @parent_io[num].closed?
+          @parent_io[num].readpartial(4096) until @parent_io[num].eof?
+        end
+      else
+        out = @parent_io[num].read unless @parent_io[num].closed?
+      end
+    end
+    [out,emptypipes]
+  end
+  # This function is made by children.
+  # It redirect the stdin,stdout,stderr
+  # Close another descriptor if we are in ruby < 2.0
+  # And launch the command with exec.
+  def run_fork()
+    begin
+      #stdin
+      STDIN.reopen(@child_io[0] || '/dev/null')
+
+      #stdout
+      STDOUT.reopen(@child_io[1] || '/dev/null')
+
+      #stderr
+      STDERR.reopen(@child_io[2] || '/dev/null')
+
+
+      # Close useless file descriptors.
+      # Since ruby 2.0, FD_CLOEXEC is set when ruby opens a descriptor.
+      # After performing exec(), all file descriptors are closed excepted 0,1,2
+      # https://bugs.ruby-lang.org/issues/5041
+      if RUBY_VERSION < "2.0"
+        Dir.foreach('/proc/self/fd') do |opened_fd|
+          begin
+            fd=opened_fd.to_i
+            if fd>2
+              f_IO=IO.new(fd)
+              f_IO.close if !f_IO.closed?
+            end
+          rescue Exception
+            #Some file descriptor are reserved for the rubyVM.
+            #So the function 'IO.new' raises an exception. We ignore that.
+          end
+        end
+      end
+      exec(*@command)
+    rescue SystemCallError, Exception => e
+      STDERR.puts "Fork Error: #{e.message} (#{e.class.name})"
+      STDERR.puts e.backtrace
+    end
+    exit! 1
   end
 end
